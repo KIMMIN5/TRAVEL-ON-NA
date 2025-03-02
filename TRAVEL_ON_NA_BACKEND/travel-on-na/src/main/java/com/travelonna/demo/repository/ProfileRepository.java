b1b3f1cd0dbde7965eacaa34f6e0196e55595b6e
LeeJungMin <130677310+JungMin-E@users.noreply.github.com>
2025-03-02 16:54:53 +0900

    Update ProfileRepository.java
    profile

--- TRAVEL_ON_NA_BACKEND/travel-on-na/src/main/java/com/travelonna/demo/repository/ProfileRepository.java
+++ TRAVEL_ON_NA_BACKEND/travel-on-na/src/main/java/com/travelonna/demo/repository/ProfileRepository.java
@@ -1,13 +1,10 @@
 package com.travelonna.demo.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.travelonna.demo.entity.Profile;
+import com.travelonna.demo.entity.User;
 
-@Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
-
-    // 프로필 저장 메서드 추가
-    Profile save(Profile profile);
+    Optional<Profile> findByUser(User user);
 }
